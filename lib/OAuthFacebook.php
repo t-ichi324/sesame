4d6c273cab291610d7d7f3929ea2eb8836b28df2
ichi <ichi@I-WORK>
2021-08-25 13:43:22 +0900

    update

--- lib/OAuthFacebook.php
+++ lib/OAuthFacebook.php
@@ -35,55 +35,53 @@ class OAuthFacebook {
             echo "<p style='color:#f00;'>ERROR : Not installed 'curl'.</p>";
             return;
         }
+        
         $param = ["code"=>$code, "client_id"=>$client_id, "client_secret"=>$client_secret, "redirect_uri" => $callback, "grant_type"=>"authorization_code"];
+        
+        $ch = curl_init(self::$URL_TOKEN);
         try{
-            $ch = curl_init(self::$URL_TOKEN);
-            try{
-                curl_setopt($ch, CURLOPT_POST, TRUE); 
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
+            curl_setopt($ch, CURLOPT_POST, TRUE); 
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
 
-                curl_setopt($ch, CURLOPT_HEADER, true);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-                $r = curl_exec($ch);
-                $info = curl_getinfo($ch);
-                $result["header"] = substr($r, 0, $info["header_size"]);
-                $result["body"] = substr($r, $info["header_size"]);
-                curl_close($ch);
-                
-                $json = json_decode($result["body"]);
-            } catch (Exception $ex) {
-                curl_close($ch);
-                $result["error"] = $ex->getMessage();
-                echo print_r($ex->getMessage());
-                $json = array();
-            }
+            $r = curl_exec($ch);
+            $info = curl_getinfo($ch);
+            //$header = substr($r, 0, $info["header_size"]);
+            $content = substr($r, $info["header_size"]);
+            curl_close($ch);
+            $json = json_decode($content);
 
-            if(empty($json) || isset($json->error)){
-                self::log($log_file, "ERR[1]", self::$URL_TOKEN, $json);
-                return null;
-            }
-            self::log($log_file, "SUCCESS", self::$URL_TOKEN, $json);
-            return $json->access_token;
-            
         } catch (Exception $ex) {
-            echo $ex->getMessage();
-            return null;
+            curl_close($ch);
+            self::log($log_file, "ERR[ getAccessToken() ]", self::$URL_TOKEN, $ex->getMessage());
+            return;
         }
 
+        if(empty($json) || isset($json->error)){
+            self::log($log_file, "ERR[ getAccessToken() ]", self::$URL_TOKEN, $json);
+            return null;
+        }
+        self::log($log_file, "SUCCESS[ getAccessToken() ]", self::$URL_TOKEN, $json);
+        return $json->access_token;
     }
     public static function getUserInfo($access_token, $log_file = null){
         if(empty($access_token)){ return null; }
         $url = self::$URL_USERI. "?" ."access_token=".$access_token."&fields=". implode(',',self::$ME_FIELDS);
-        
-        $re = file_get_contents($url);
-        $json = json_decode($re);
-        if(empty($json) || isset($json->error)){
-            self::log($log_file, "ERR[2]", $url, $json);
+        try{
+            $re = file_get_contents($url);
+        } catch (Exception $ex) {
+            self::log($log_file, "ERR[ getUserInfo() ]", $url, $ex->getMessage());
             return null;
         }
-        self::log($log_file, "SUCCESS", $url, $json);
+        $json = json_decode($re);
+        if(empty($json) || isset($json->error)){
+            self::log($log_file, "ERR[ getUserInfo() ]", $url, $json);
+            return null;
+        }
+        self::log($log_file, "SUCCESS[ getUserInfo() ]", $url, $json);
         return $json;
     }
     
@@ -91,17 +89,17 @@ class OAuthFacebook {
         if(empty($log_file)) { return; }
         try{
             $line = date("Y-m-d H:i:s")."\t" .
+                    $msg."\t" .
                     $_SERVER["REMOTE_ADDR"]."\t" .
                     $_SERVER["REQUEST_METHOD"]."\t" .
                     $_SERVER["REQUEST_URI"]."\t" .
-                    $msg."\t" .
                     session_id();
             $line .= "\t".(isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "");
             if(!empty($api)){
                 if(is_array($json) || is_object($json)){
-                    $line .= "\n >>> ".$api."\t" .json_encode($json);
+                    $line .= "\n >>> ".$api."\n >>> " .json_encode($json);
                 }else{
-                    $line .= "\n >>> ".$api."\t" .$json;
+                    $line .= "\n >>> ".$api."\n >>> " .$json;
                 }
             }
             error_log($line."\n", 3, $log_file);
--- lib/OAuthGoogle.php
+++ lib/OAuthGoogle.php
@@ -21,13 +21,11 @@ class OAuthGoogle {
     private static $URL_AUTH = 'https://accounts.google.com/o/oauth2/auth';
     private static $URL_TOKEN = 'https://accounts.google.com/o/oauth2/token';
     private static $URL_USERI = 'https://www.googleapis.com/oauth2/v1/userinfo';
+    private static $OAUTH_SCOPE = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'];
     
     public static function getUrlAuth($client_id, $callback){
-        $scope = array(
-            'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/userinfo.profile'
-        );
-        $param = array( 'client_id' => $client_id, 'redirect_uri'=>$callback,'scope' => implode(' ', $scope), 'response_type' => 'code');
+        $param = array( 'client_id' => $client_id, 'redirect_uri'=>$callback, 'response_type' => 'code');
+        $param["scope"] = implode(' ',self::$OAUTH_SCOPE);
         return self::$URL_AUTH."?".http_build_query($param);
     }
 
@@ -37,72 +35,70 @@ class OAuthGoogle {
             return;
         }
         $param = ["code"=>$code, "client_id"=>$client_id, "client_secret"=>$client_secret, "redirect_uri" => $callback, "grant_type"=>"authorization_code"];
+        
+        $ch = curl_init(self::$URL_TOKEN);
         try{
-            $ch = curl_init(self::$URL_TOKEN);
-            try{
-                curl_setopt($ch, CURLOPT_POST, TRUE); 
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
+            curl_setopt($ch, CURLOPT_POST, TRUE); 
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
 
-                curl_setopt($ch, CURLOPT_HEADER, true);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-                $r = curl_exec($ch);
-                $info = curl_getinfo($ch);
-                $result["header"] = substr($r, 0, $info["header_size"]);
-                $result["body"] = substr($r, $info["header_size"]);
-                curl_close($ch);
-                
-                $json = json_decode($result["body"]);
-            } catch (Exception $ex) {
-                curl_close($ch);
-                $result["error"] = $ex->getMessage();
-                echo print_r($ex->getMessage());
-                $json = array();
-            }
+            $r = curl_exec($ch);
+            $info = curl_getinfo($ch);
+            //$header = substr($r, 0, $info["header_size"]);
+            $content = substr($r, $info["header_size"]);
+            curl_close($ch);
+            $json = json_decode($content);
 
-            if(empty($json) || isset($json->error)){
-                self::log($log_file, "ERR[1]", self::$URL_TOKEN, $json);
-                return null;
-            }
-            self::log($log_file, "SUCCESS", self::$URL_TOKEN, $json);
-            return $json->access_token;
-            
         } catch (Exception $ex) {
-            echo $ex->getMessage();
-            return null;
+            curl_close($ch);
+            self::log($log_file, "ERR[ getAccessToken() ]", self::$URL_TOKEN, $ex->getMessage());
+            return;
         }
 
+        if(empty($json) || isset($json->error)){
+            self::log($log_file, "ERR[ getAccessToken() ]", self::$URL_TOKEN, $json);
+            return null;
+        }
+        self::log($log_file, "SUCCESS[ getAccessToken() ]", self::$URL_TOKEN, $json);
+        return $json->access_token;
     }
+    
     public static function getUserInfo($access_token, $log_file = null){
         if(empty($access_token)){ return null; }
         $url = self::$URL_USERI. "?"."access_token=".$access_token;
-        
-        $re = file_get_contents($url);
-        $json = json_decode($re);
-        if(empty($json) || isset($json->error)){
-            self::log($log_file, "ERR[2]", $url, $json);
+        try{
+            $re = file_get_contents($url);
+        } catch (Exception $ex) {
+            self::log($log_file, "ERR[ getUserInfo() ]", $url, $ex->getMessage());
             return null;
         }
-        self::log($log_file, "SUCCESS", $url, $json);
+        $json = json_decode($re);
+        if(empty($json) || isset($json->error)){
+            self::log($log_file, "ERR[ getUserInfo() ]", $url, $json);
+            return null;
+        }
+        self::log($log_file, "SUCCESS[ getUserInfo() ]", $url, $json);
         return $json;
-    }
+    }    
     
     private static function log($log_file, $msg, $api, $json){
         if(empty($log_file)) { return; }
         try{
             $line = date("Y-m-d H:i:s")."\t" .
+                    $msg."\t" .
                     $_SERVER["REMOTE_ADDR"]."\t" .
                     $_SERVER["REQUEST_METHOD"]."\t" .
                     $_SERVER["REQUEST_URI"]."\t" .
-                    $msg."\t" .
                     session_id();
             $line .= "\t".(isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : "");
             if(!empty($api)){
                 if(is_array($json) || is_object($json)){
-                    $line .= "\n >>> ".$api."\t" .json_encode($json);
+                    $line .= "\n >>> ".$api."\n >>> " .json_encode($json);
                 }else{
-                    $line .= "\n >>> ".$api."\t" .$json;
+                    $line .= "\n >>> ".$api."\n >>> " .$json;
                 }
             }
             error_log($line."\n", 3, $log_file);
